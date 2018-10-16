6b33e0e0b5e894b9741ae08f8e5317d11f8fbdbf
Beatriz Marouelli <biamarou@gmail.com>
2018-10-16 14:03:55 +0000

    Add the project platform
    Signed-off-by: Beatriz Marouelli <biamarou@gmail.com>

---
+++ geocomp-py-framework/geocomp/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""Algoritmos de Geometria Computacional
+
+Sub-modulos:
+- convexhull: algoritmos para o problema do Fecho Convexo bidimensional
+- farthest:   algoritmos para encontrar o par de pontos mais distante
+
+- common:     classes e operacoes usadas por diversos algoritmos
+- gui:        implementacoes das operacoes graficas
+"""
+
+from . import convexhull
+from . import farthest
+from .common.guicontrol import init_display
+from .common.guicontrol import config_canvas
+from .common.guicontrol import run_algorithm
+from .common.io import open_file
+from .common.prim import get_count
+from .common.prim import reset_count
+
+children = (   ( 'convexhull', None, 'Fecho Convexo' ),
+		( 'farthest',  None, 'Par Mais Distante' )
+	)
+
+__all__ = [p[0] for p in children]
