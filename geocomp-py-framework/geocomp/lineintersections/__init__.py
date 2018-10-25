eeec2821e1d915e1157026a943f6bc2e089e66b6
Beatriz Marouelli <biamarou@gmail.com>
2018-10-25 19:15:08 +0000

    Fixes the bug of the children array
    Signed-off-by: Beatriz Marouelli <biamarou@gmail.com>

--- geocomp-py-framework/geocomp/lineintersections/__init__.py
+++ geocomp-py-framework/geocomp/lineintersections/__init__.py
@@ -1,8 +1,5 @@
 from . import brute_force
 
-children = (
-    ('brute_force', 'Brute_force', 'Forca\nBruta'),
-    ('', '', '')
-)
+children = [('brute_force', 'Brute_force', 'Forca\nBruta')]
 
 __all__ = [a[0] for a in children]
