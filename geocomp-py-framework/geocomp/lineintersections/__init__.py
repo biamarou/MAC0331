a83e5502cdbe2cd3cb34eeb7ac7f65164a1e2c64
Beatriz Marouelli <biamarou@gmail.com>
2018-11-05 15:40:58 +0000

    Add the Bentley & Ottman algorithm to find line intersections
    Signed-off-by: Beatriz Marouelli <biamarou@gmail.com>

--- geocomp-py-framework/geocomp/lineintersections/__init__.py
+++ geocomp-py-framework/geocomp/lineintersections/__init__.py
@@ -1,5 +1,6 @@
 from . import brute_force
+from . import bentley_ottman
 
-children = [('brute_force', 'Brute_force', 'Forca\nBruta')]
+children = [('brute_force', 'Brute_force', 'Forca\nBruta'), ('bentley_ottman', 'Bentley_ottman', 'Bentley & Ottman')]
 
 __all__ = [a[0] for a in children]
