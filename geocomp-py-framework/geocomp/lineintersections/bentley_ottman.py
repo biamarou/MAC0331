9fcac5b4b9feda5a60c484974a14aa12563c5230
Beatriz Marouelli <biamarou@gmail.com>
2018-11-14 19:12:42 +0000

    Add lots of debug prints (they are commented and won't interfere with the execution)
    Signed-off-by: Beatriz Marouelli <biamarou@gmail.com>

--- geocomp-py-framework/geocomp/lineintersections/bentley_ottman.py
+++ geocomp-py-framework/geocomp/lineintersections/bentley_ottman.py
@@ -1,10 +1,12 @@
 from geocomp.common import prim
 from geocomp.common import segment
 from geocomp.common import control
+from geocomp.common import point
 from geocomp import config
-from geocomp.lineintersections import PointBST
-from geocomp.lineintersections import SegmentBST
+from geocomp.lineintersections import PointBST as PBST
+from geocomp.lineintersections import SegmentBST as SBST 
 
+sleep_time = 2.0
 
 def Bentley_ottman (l):
     segments_list, points_list = filter_segments_and_points(l) #s is the segments list and p is the points list
@@ -13,33 +15,142 @@ def Bentley_ottman (l):
     for s in segments_list:
         s.plot()
 
-    event_queue = PointBST()
+    event_queue = PBST.PointBST()
     for p in points_list:
-        event_queue.insert(p[0], segments_list[p[1]])
+        event_queue.insert(p[0], [segments_list[p[1]]])
 
-    segment_tree = SegmentBST()
+    segment_tree = SBST.SegmentBST()
     while(not event_queue.isEmpty()):
         p = event_queue.removeMinKey()
-        if(p.segment.init == p.key): #p eh ponta esquerda
-            segment_tree.insert(p.segment)
-            pred = segment_tree.get_predecessor(p.segment, False) #mando False pois nao estou removendo o segmento
-            succ = segment_tree.get_sucessor(p.segment, False)
-            if(#intersectam)
+        #print("sweep line")
+        #print(p.key.x)
+        #print(p.key.y)
 
-        else: #p eh ponta direita
-            pred = segment_tree.get_predecessor(p.segment, True)
-            succ = segment_tree.get_sucessor(p.segment, True)
-            segment_tree.remove(p.segment)
-            if(#intersectam)
-        #ESTAVAMOS AQUI !!!!
+        if (p.segment[0].init == p.key): #p eh ponta esquerda
+            p.key.hilight(color = "blue")
+            control.sleep(sleep_time)
+            segment_tree.insert(p.segment[0], p.key)
+
+            pred = segment_tree.get_predecessor(p.segment[0], p.key)
+            succ = segment_tree.get_sucessor(p.segment[0], p.key)
+
+            if (not pred and not succ): continue
+            if (pred): pred.key.hilight(color_line = "magenta")
+            if (succ): succ.key.hilight(color_line = "magenta")
+            control.sleep(sleep_time)
+
+            if (pred and prim.intersect(pred.key.init, pred.key.to, p.segment[0].init, p.segment[0].to)):
+                pred.key.hilight(color_line = "yellow")
+                p.segment[0].hilight(color_line = "yellow")
+                control.sleepsweepline_point(sleep_time)
+                p.segment[0].plot()
+                pred.key.plot()      
+                intersections.append((pred.key, p.segment[0]))
+                x,y = get_intersection(pred.key, p.segment[0])
+                #print("intersecao")
+                #print(x)
+                #print(y)
+                event_queue.insert(point.Point(x, y), [pred.key, p.segment[0]])
+               
+            if (succ and prim.intersect(succ.key.init, succ.key.to, p.segment[0].init, p.segment[0].to)):
+                succ.key.hilight(color_line = "yellow")
+                p.segment[0].hilight(color_line = "yellow")
+                control.sleep(sleep_time)
+                p.segment[0].plot()
+                succ.key.plot()
+                intersections.append((succ.key, p.segment[0]))
+                x,y = get_intersection(succ.key, p.segment[0])
+                #print("intersecao")
+                #print(x)
+                #print(y)
+                event_queue.insert(point.Point(x, y), [p.segment[0], succ.key])
+
+            if (pred): pred.key.plot()
+            if (succ): succ.key.plot()
+            p.segment[0].init.plot()
+
+        elif (p.segment[0].to == p.key): #p eh ponta direita
+            p.segment[0].to.hilight(color = "blue")
+            control.sleep(sleep_time)
+
+            pred = segment_tree.get_predecessor(p.segment[0], p.key)
+            succ = segment_tree.get_sucessor(p.segment[0], p.key)
+
+            if(not pred or not succ): continue
+
+            segment_tree.remove(p.segment[0], p.key)
+            
+            pred.key.hilight(color_line = "magenta")
+            succ.key.hilight(color_line = "magenta")
+            control.sleep(sleep_time)
+            
+            if (prim.intersect(pred.key.init, pred.key.to, succ.key.init, succ.key.to)):
+                succ.key.hilight(color_line = "yellow")
+                pred.key.hilight(color_line = "yellow")
+                control.sleep(sleep_time)
+                pred.key.plot()
+                succ.key.plot()
+                intersections.append((pred.key, succ.key))
+                x,y = get_intersection(pred.key, succ.key)
+                event_queue.insert(Point(x, y), [pred.key, succ.key])
+            
+            pred.key.plot()
+            succ.key.plot()
+            p.segment[0].to.plot()
+        
+        else: # ponto de intersecao
+            #print("ELSE")
+            p.key.plot()
+            p.key.hilight(color = "blue")
+            control.sleep(sleep_time)
+            p.key.plot()
+            #print("tree before remove/insert")
+            segment_tree.imprime()
+            segment_tree.remove(p.segment[0], p.key)
+            #print("remove")
+            segment_tree.remove(p.segment[1], p.key)
+            
+            #print("tree after remove")
+            segment_tree.imprime()
+            segment_tree.insert(p.segment[1], p.key)
+            segment_tree.insert(p.segment[0], p.key)
+            
+            #print("tree after insert")
+            segment_tree.imprime()
+
+
+
 
 def filter_segments_and_points (l):
     segments = []
     points = []
+    seg_index = 0
+    
     for i in range (0, len(l) - 1, 2):
-        segments.append(segment.Segment(l[i], l[i + 1]))
-        points.append((l[i], i))
-        points.append((l[i + 1], i))
+        if (l[i].x > l[i + 1].x):
+            init, to = l[i+1], l[i]
+        else:
+            init, to = l[i], l[i+1]
 
+        segments.append(segment.Segment(init, to))
+        points.append([init, seg_index])
+        points.append([to, seg_index])
+        seg_index += 1
     return segments, points
+
+
+def get_intersection(r, s):
+    # quando a reta estah na forma 'y = ax + b' o coef a eh delta
+
+    delta_r = (r.init.y - r.to.y)/(r.init.x - r.to.x)
+    delta_s = (s.init.y - s.to.y)/(s.init.x - s.to.x)
+
+    b_r = -(delta_r * r.init.x) + r.init.y
+    b_s = -(delta_s * s.init.x) + s.init.y
+
+    x = -(b_r - b_s)/(delta_r - delta_s)
+    y = delta_r * (x - r.init.x) + r.init.y
+
+    return (x, y)
+
 
