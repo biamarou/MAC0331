c38daa83274b1cdf4d1bca58d25d341cef27570a
Beatriz Marouelli <biamarou@gmail.com>
2018-11-14 19:06:50 +0000

    Fix the orientation of the functions that use 'compare_to', add the segment tree print function
    Signed-off-by: Beatriz Marouelli <biamarou@gmail.com>

--- geocomp-py-framework/geocomp/lineintersections/PointBST.py
+++ geocomp-py-framework/geocomp/lineintersections/PointBST.py
@@ -13,40 +13,40 @@ class PointBST:
             self.segment = sg
 
     def __init__(self):
-        self.root = NULL
+        self.root = None
 
     def isEmpty(self):
-        return self.root == NULL
+        return self.root == None
     
     def size (self):
-        return size(self.root)
+        return size_aux(self.root)
     
-    def size(self, n):
-        if (n == NULL): 
+    def size_aux(self, n):
+        if (n == None): 
             return 0
         return n.size
 
     def height (self):
-        return height(self.root)
+        return height_aux(self.root)
     
-    def height(self, n):
-        if (n == NULL): 
+    def height_aux(self, n):
+        if (n == None): 
             return -1
         return n.height
 
     def removeMinKey(self):
         node = self.root
-        while (node.left != NULL):
+        while (node.left != None):   
             node = node.left
         self.remove(node.key)
-        return node.key
+        return node
     
     def insert (self, point, segment):
         #We pass the segments of which the point is a vertice
-        if (self.root == NULL):
-            self.root = Node(point, 0, 1, NULL, NULL, segment)    
+        if (self.root == None):
+            self.root = self.Node(point, 0, 1, None, None, segment)    
         else:
-            self.root = self.insert(self.root, point)
+            self.root = self.insert_aux(self.root, point, segment)
     
     def compare_to (self, p1, p2):
         if (p1.x > p2.x):
@@ -56,21 +56,26 @@ class PointBST:
         return 0
 
     # Assuming there aren't two equal points
-    def insert (self, node, point):
-        if (node == NULL):
-            return Node(point, 0, 1, NULL, NULL)
-        cmp = self.compare_to(point, node.key)
-        if (cmp < 0):
-            node.left = self.insert(node.left, point)
-        elif (cmp > 0):
-            node.right = self.insert(node.right, point)
-
-        node.size = 1 + self.size(node.left) + self.size(node.right)
-        node.height = 1 + max(self.height(node.left), self.height(node.right))
-        return balance(node)
+    def insert_aux (self, node, point, segment):
+        if (node == None):
+            return self.Node(point, 0, 1, None, None, segment)
+        cmp = self.compare_to(node.key, point)
+        if (cmp > 0):
+            node.left = self.insert_aux(node.left, point, segment)
+        elif (cmp < 0):
+            node.right = self.insert_aux(node.right, point, segment)
+        else:
+            if (node.key.y > point.y):
+               node.left = self.insert_aux(node.left, point, segment)
+            else:
+                node.right = self.insert_aux(node.right, point, segment)
+                
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        return self.balance(node)
 
     def balance_factor(self, node):
-        return self.height(node.left) - self.height(node.right)
+        return self.height_aux(node.left) - self.height_aux(node.right)
 
     def balance (self, node):
         if (self.balance_factor(node) < -1):
@@ -90,9 +95,9 @@ class PointBST:
         node.left = node2.right
         node2.right = node
         node2.size = node.size
-        node.size = 1 + self.size(node.left) + self.size(node.right)
-        node.height = 1 + max(self.height(node.left), self.height(node.right))
-        node2.height = 1 + max(self.height(node2.left), self.height(node2.right))
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        node2.height = 1 + max(self.height_aux(node2.left), self.height_aux(node2.right))
         return node2
 
     def rotate_left(self, node):
@@ -100,37 +105,77 @@ class PointBST:
         node.right = node2.left
         node2.left = node
         node2.size = node.size
-        node.size = 1 + self.size(node.left) + self.size(node.right)
-        node.height = 1 + max(self.height(node.left), self.height(node.right))
-        node2.height = 1 + max(self.height(node2.left), self.height(node2.right))
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        node2.height = 1 + max(self.height_aux(node2.left), self.height_aux(node2.right))
         return node2
 
     def contains(self, point):
-        if (point == NULL) return False
-        return self.contains(self.root, point)
+        if (point == None): return False
+        return self.contains_aux(self.root, point)
 
-    def contains(self, node, point):
-        if (node == NULL):
+    def contains_aux(self, node, point):
+        if (node == None):
             return False
-        cmp = self.compare_to(point, node.key)
-        if (cmp < 0):
-            node.left = self.contains(node.left, point)
-        elif (cmp > 0):
-            node.right = self.contains(node.right, point)
+        cmp = self.compare_to(node.key, point)
+        if (cmp > 0):
+            return self.contains_aux(node.left, point)
+        elif (cmp < 0):
+            return self.contains_aux(node.right, point)
         else:
-             return True
+            if (node.key.y > point.y):
+                return self.contains_aux(node.left, point)
+            elif (node.key.y < point.y):
+                return self.contains_aux(node.right, point)
+            else:
+                return True
 
 
     def remove(self, point):
         if (self.contains(point)):
-            self.root = self.remove(self.root, point)
+            self.root = self.remove_aux(self.root, point)
 
     #Assuming there aren`t two equal points  
-    def remove(self, node, point):
-        cmp = self.compare_to(point, node.key)
-        if (cmp < 0) node.left = self.remove(node.left, point)
-        elif (cmp > 0) node.right = self.remove(node.right, point)
+    def remove_aux(self, node, point):
+        cmp = self.compare_to(node.key, point)
+        if (cmp > 0): node.left = self.remove_aux(node.left, point)
+        elif (cmp < 0): node.right = self.remove_aux(node.right, point)
+        else:
+                if (node.key.y > point.y):
+                    node.left = self.remove_aux(node.left, point)
+                elif (node.key.y < point.y):
+                    node.right = self.remove_aux(node.right, point)
+                else:
+                    if (node.left == None):
+                        return node.right
+                    elif(node.right == None):
+                        return node.left
+                    else:
+                        node_y = self.Node(node.key, node.height, node.size, node.right, node.left, node.segment)
+                        node = self.min_aux(node_y.right)
+                        node.right = self.remove_min_aux(node_y.right)
+                        node.left = node_y.left  
         
-        node.size = 1 + self.size(node.left) + self.size(node.right)
-        node.height = 1 + max(self.height(node.left), self.height(node.right))
-        return balance(node)
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        return self.balance(node)
+
+    def remove_min(self):
+        if(not self.isEmpty()):
+            self.root = self.remove_min_aux(self.root)
+
+    def remove_min_aux(self, node):
+        if (node.left == None): return node.right
+        node.left = self.remove_min_aux(node.left)
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        return self.balance(node)
+
+    def min(self):
+        if (not self.isEmpty()):
+            return self.min_aux(self.root)
+
+    def min_aux(self, node):
+        if(node.left == None):
+            return node
+        return self.min_aux(node.left)
--- geocomp-py-framework/geocomp/lineintersections/SegmentBST.py
+++ geocomp-py-framework/geocomp/lineintersections/SegmentBST.py
@@ -1,4 +1,5 @@
 from geocomp.common import segment
+from geocomp.common import prim
 
 class SegmentBST:
     
@@ -12,60 +13,60 @@ class SegmentBST:
             self.left = l
 
     def __init__(self):
-        self.root = NULL
+        self.root = None
 
     def isEmpty(self):
-        return self.root == NULL
+        return self.root == None
     
     def size (self):
-        return size(self.root)
+        return size_aux(self.root)
     
-    def size(self, n):
-        if (n == NULL): 
+    def size_aux(self, n):
+        if (n == None): 
             return 0
         return n.size
 
     def height (self):
-        return height(self.root)
+        return height_aux(self.root)
     
-    def height(self, n):
-        if (n == NULL): 
+    def height_aux(self, n):
+        if (n == None): 
             return -1
         return n.height
-    
-    def insert (self, segment):
-        if (self.root == NULL):
-            self.root = Node(segment, 0, 1, NULL, NULL)    
-        else:
-            self.root = self.insert(self.root, segment)
     
     def get_y_coord (self, l, r, x):
         return ((r.y - l.y)/(r.x - l.x)) * (x - (l.x)) + l.y
 
-    def compare_to (self, s, p):
-        y = get_y_coord(s.init, s.to, p.x)
-        if (y > p.y):
-            return 1
-        elif (y <= p.y):
-            return -1
-        return 0
+    def compare_to (self, p, s): #devolve 1 se p estah ah esquerda de s.
+        left_test = prim.left(s.init, s.to, p)
+        left_on_test = prim.left_on(s.init, s.to, p)
+
+        if (left_test): return 1
+        elif (left_on_test): return 0
+        else: return -1
+
+    def insert (self, segment, sweepline_point): #sweepline_point eh a x-coordenada da sweepline
+        if (self.root == None):
+            self.root = self.Node(segment, 0, 1, None, None)    
+        else:
+            self.root = self.insert_aux(self.root, segment, sweepline_point)
 
     # Assuming there aren't two equal segments
-    def insert (self, node, segment):
-        if (node == NULL):
-            return Node(segment, 0, 1, NULL, NULL)
-        cmp = self.compare_to(segment, node.key.init)
-        if (cmp < 0):
-            node.left = self.insert(node.left, segment)
+    def insert_aux (self, node, segment, sweepline_point):
+        if (node == None):
+            return self.Node(segment, 0, 1, None, None)
+        cmp = self.compare_to(sweepline_point, node.key)
+        if (cmp <= 0):
+            node.left = self.insert_aux(node.left, segment, sweepline_point)
         elif (cmp > 0):
-            node.right = self.insert(node.right, segment)
+            node.right = self.insert_aux(node.right, segment, sweepline_point)
 
-        node.size = 1 + self.size(node.left) + self.size(node.right)
-        node.height = 1 + max(self.height(node.left), self.height(node.right))
-        return balance(node)
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        return self.balance(node)
 
     def balance_factor(self, node):
-        return self.height(node.left) - self.height(node.right)
+        return self.height_aux(node.left) - self.height_aux(node.right)
 
     def balance (self, node):
         if (self.balance_factor(node) < -1):
@@ -85,9 +86,9 @@ class SegmentBST:
         node.left = node2.right
         node2.right = node
         node2.size = node.size
-        node.size = 1 + self.size(node.left) + self.size(node.right)
-        node.height = 1 + max(self.height(node.left), self.height(node.right))
-        node2.height = 1 + max(self.height(node2.left), self.height(node2.right))
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        node2.height = 1 + max(self.height_aux(node2.left), self.height_aux(node2.right))
         return node2
 
     def rotate_left(self, node):
@@ -95,119 +96,150 @@ class SegmentBST:
         node.right = node2.left
         node2.left = node
         node2.size = node.size
-        node.size = 1 + self.size(node.left) + self.size(node.right)
-        node.height = 1 + max(self.height(node.left), self.height(node.right))
-        node2.height = 1 + max(self.height(node2.left), self.height(node2.right))
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        node2.height = 1 + max(self.height_aux(node2.left), self.height_aux(node2.right))
         return node2
 
-    def contains(self, segment):
-        if (segment == NULL) return False
-        return self.contains(self.root, segment)
+    def contains(self, segment, sweepline_point):
+        if (segment == None): return False
+        return self.contains_aux(self.root, segment, sweepline_point)
 
-    def contains(self, node, segment):
-        if (node == NULL):
+    def contains_aux(self, node, segment, sweepline_point):
+        #print("to procurando")
+        if (node == None):
             return False
-        cmp = self.compare_to(segment, node.key)
-        if (cmp < 0):
-            node.left = self.contains(node.left, segment)
+        cmp = self.compare_to(sweepline_point, node.key)
+        #print(cmp)
+        if (cmp < 0 or (cmp == 0 and segment != node.key)):
+            #print("achei primeiro seg")
+            self.contains_aux(node.left, segment, sweepline_point)
         elif (cmp > 0):
-            node.right = self.contains(node.right, segment)
+            self.contains_aux(node.right, segment, sweepline_point)
         else:
              return True
 
-    def get_predecessor(self, key, remove): 
-    #    se o noh tem subarvore direita :min(subarvore direita)
-    #                       cc se sou filho esquerdo, meu pai eh antecessor
+    def get_predecessor(self, key, sweepline_point): 
+    #    se o noh tem subarvore esquerda :min(subarvore esquerda)
+    #                       cc se sou filho direito, meu pai eh antecessor
     #                       cc nao tenho antecessor
-        node_dad = self.root
-        p = key.init
-            
-        if (remove):
-            p = node.key.to
+        node = self.root
+        node_dad = None
+        
+        while (node != None):
 
-        if (node_dad == NULL): return False
+            cmp = self.compare_to(sweepline_point, node.key)
 
-        while (node_dad != NULL):
-            if(self.compare_to(node_dad.key, p) > 0):
-                if (node_dad.left.key == key):
-                    break
-                node_dad = node_dad.left
+            if(cmp > 0):
+                node_dad = node
+                node = node.right
+            elif(cmp < 0 or (cmp == 0 and key != node.key)):
+                node_dad = node
+                node = node.left
             else:
-                if (node_dad.right.key == key):
-                    break
-                node_dad = node_dad.right
+                break
 
-        if (node_dad == NULL): return False
-
-        #vale que node_dad eh o pai e node_dad.left ou right eh o node
-        if(node_dad.left.key == key)
-            node = node_dad.left
+        if (node.left != None): 
+            return self.max_aux(node.left)
         else:
-            node = node_dad.right
-
-        if (node.right != NULL):
-            node_min = node.right
-            while (node_min.left != NULL):
-                node_min = node.left
-            return node_min
-
-        else:
-            if(node_dad.left == node):
-                return node_dad
+            if (node_dad != None):
+                if(node_dad.right != None and node_dad.right.key == key):
+                    return node_dad
         return False
 
-    def get_sucessor(self, node, remove): 
-    #    se o noh tem subarvore esquerda :max(subarvore esquerda)
-    #                       cc se sou filho direito, meu pai eh sucessor
+    def get_sucessor(self, key, sweepline_point): 
+    #    se o noh tem subarvore direita :max(subarvore direita)
+    #                       cc se sou filho esquerdo, meu pai eh sucessor
     #                       cc nao tenho sucessor
-        node_dad = self.root
-        p = key.init
-            
-        if (remove):
-            p = node.key.to
+        node = self.root
+        node_dad = None
 
-        if (node_dad == NULL): return False
+        while (node != None):
 
-        while (node_dad != NULL):
-            if(self.compare_to(node_dad.key, p) > 0):
-                if (node_dad.left.key == key):
-                    break
-                node_dad = node_dad.left
+            cmp = self.compare_to(sweepline_point, node.key)
+            if(cmp > 0):
+                node_dad = node
+                node = node.right
+            elif(cmp < 0 or (cmp == 0 and key != node.key)):
+                node_dad = node
+                node = node.left
             else:
-                if (node_dad.right.key == key):
-                    break
-                node_dad = node_dad.right
+                break
 
-        if (node_dad == NULL): return False
-
-        #vale que node_dad eh o pai e node_dad.left ou right eh o node
-        if(node_dad.left.key == key)
-            node = node_dad.left
+        if (node.right != None):
+            return self.min_aux(node.right)
         else:
-            node = node_dad.right
-
-        if (node.left != NULL):
-            node_max = node.left
-            while (node_max.right != NULL):
-                node_max = node.right
-            return node_max
-
-        else:
-            if(node_dad.right == node):
-                return node_dad
+            if (node_dad != None):
+                if(node_dad.left != None and node_dad.left.key == key):
+                    return node_dad
         return False
 
 
-    def remove(self, segment):
-        if (self.contains(segment)):
-            self.root = self.remove(self.root, segment)
+    def remove(self, segment, sweepline_point):
+        if (self.contains(segment, sweepline_point)):
+            #print("There is a point, we should remove it!")
+            self.root = self.remove_aux(self.root, segment, sweepline_point)
 
     #Assuming there aren`t two equal segments  
-    def remove(self, node, segment):
-        cmp = self.compare_to(segment, node.key.init)
-        if (cmp < 0) node.left = self.remove(node.left, segment)
-        elif (cmp > 0) node.right = self.remove(node.right, segment)
+    def remove_aux(self, node, segment, sweepline_point):
         
-        node.size = 1 + self.size(node.left) + self.size(node.right)
-        node.height = 1 + max(self.height(node.left), self.height(node.right))
-        return balance(node)
+        cmp = self.compare_to(sweepline_point, node.key)
+        
+        if (cmp < 0 or (cmp == 0 and segment != node.key)): 
+            node.left = self.remove_aux(node.left, segment, sweepline_point)
+        elif (cmp > 0): node.right = self.remove_aux(node.right, segment, sweepline_point)
+        else:
+            if (node.left == None):
+                return node.right
+    
+            elif (node.right == None):
+                return node.left
+    
+            else:
+                p = self.Node(node.key, node.height, node.size, node.right, node.left)
+                node = self.min_aux(p.right)
+                node.right = self.remove_min_aux(p.right)
+                node.left = p.left  
+        
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        return self.balance(node)
+
+    def remove_min(self):
+        if(not self.isEmpty()):
+            self.root = self.remove_min_aux(self.root)
+
+    def remove_min_aux(self, node):
+        if (node.left == None): return node.right
+        node.left = self.remove_min_aux(node.left)
+        node.size = 1 + self.size_aux(node.left) + self.size_aux(node.right)
+        node.height = 1 + max(self.height_aux(node.left), self.height_aux(node.right))
+        return self.balance(node)
+
+    def min(self):
+        if (not self.isEmpty()):
+            return self.min_aux(self.root)
+
+    def min_aux(self, node):
+        if(node.left == None):
+            return node
+        return self.min_aux(node.left)
+    
+    def max_aux(self, node):
+        if(node.right == None):
+            return node
+        return self.max_aux(node.right)
+
+    def imprime(self):
+        self.imprime_aux(self.root)
+    
+    def imprime_aux(self, node):
+        if (node == None):
+            return
+        self.imprime_aux(node.left)
+        self.imprime_seg(node.key)
+        self.imprime_aux(node.right)
+
+    def imprime_seg(self, s):
+        segment = 'init ' + str([s.init.x, s.init.y]) + ' to ' + str([s.to.x, s.to.y]) 
+        print(segment)
